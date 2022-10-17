158d71d1c4ae272dd7b290ec56bd2dbf81a807d9
nebar27 <nelsonabelbi@gmail.com>
2022-10-17 16:54:36 -0500

    First commit to create an app manually without using story board

---
+++ conctactsAppManually/ContactsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContactsViewController.swift
+//  conctactsAppManually
+//
+//  Created by NELSON ABEL  BARRANZUELA IMAN on 16/10/22.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        print("loading ContactsViewController ...")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
