a64e30c8e18d4d3fd58a36b8d17fee4c640710c1
nebar27 <nelsonabelbi@gmail.com>
2022-10-16 21:38:22 -0500

    Initial Commit

---
+++ conctactsAppManually/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  conctactsAppManually
+//
+//  Created by NELSON ABEL  BARRANZUELA IMAN on 16/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
